7062d0ee1e77ebc6af2a5988f57aa7307501d52e
alverson22 <alverson_22@yahoo.com.tw>
2017-08-11 13:42:10 +0800

    Layout upgrade and add notification

--- BlankCordovaApp2/www/scripts/index.js
+++ BlankCordovaApp2/www/scripts/index.js
@@ -17,12 +17,13 @@
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        window.plugins.PushbotsPlugin.initialize("5989527b4a9efa19fc8b4567", { "android": { "sender_id": "784353059382" } });
         $("#initial").fadeOut("slow");
         $("#loaded").fadeIn("slow");
         $("#discovered").fadeIn("slow");
     };
 
-     function discovered() {
+    function discovered() {
         $("#discovered").fadeIn("slow");
         $("#record").fadeOut("slow");
         $("#add").fadeOut("slow");
@@ -38,13 +39,36 @@
         $("#discovered").fadeOut("slow");
         $("#record").fadeOut("slow");
         $("#add").fadeIn("slow");
-
-
     }
 
-    
 
-   
+    function addNotif(vendorID, title, iconURL, information, URL) {
+        $("notifList").append(
+            "<ul class='event- list'>" + 
+                "< li >" +
+                "<time datetime=''>" +
+                    "<span class='vendor'>" + vendorID + "</span>" +
+                "</time>" +
+                "<img src='" + iconURL + "' />" +
+                "<div class='info'>" +
+                    "<h2 class='title'>" + title + "</h2>" +
+                    "<p class='desc'>" + information + "</p>" +
+                    "<ul>" +
+                        "<li style='width:50%;'><a href='" + URL + "'><span class='fa fa-globe'></span>Website</a></li>" +
+                    "</ul>" +
+                "</div>" +
+                "<div class='social'>" +
+                    "<ul>" +
+                        "<li class='facebook' style='width:33%;'><a href='#facebook'><span class='fa fa-facebook'></span></a></li>" +
+                        "<li class='twitter' style='width:34%;'><a href='#twitter'><span class='fa fa-twitter'></span></a></li>" +
+                        "<li class='google-plus' style='width:33%;'><a href='#google-plus'><span class='fa fa-google-plus'></span></a></li>" +
+                   " </ul>" +
+                "</div>" +
+                "</li>" +
+                       "</ul >");
+    }
+
+
     function onPause() {
         // TODO: 這個應用程式已暫停。請在這裡儲存應用程式狀態。
     };
@@ -59,8 +83,8 @@
         document.getElementById("recordBtn").addEventListener("click", record, false);
         document.getElementById("addBtn").addEventListener("click", add, false);
 
-        
 
+        //closing the img modal by clicking nowhere
         $("#modalFull").click(function () {
             document.getElementById("modalFull").style.display = "none";
         })
@@ -68,7 +92,7 @@
         var m_src;
         var m_alt;
         // Modal Image Gallery
-        $("img").click(function () {
+        $(".discovered > .discovered-block > .discovered-middle > img").click(function () {
             document.getElementById("imgFull").src = this.src;
             document.getElementById("modalFull").style.display = "block";
             var captionText = document.getElementById("caption");
@@ -77,15 +101,45 @@
             m_alt = this.alt;
         })
 
+        //append my exhibition
         document.getElementById("signIn").addEventListener("click", function () {
             document.getElementById("myExhibition").innerHTML += '<div class="discovered-item" >' +
                 '<img src=' + m_src + ' class="img-rounded img-responsive" alt=' + m_alt + ' />'
         }, false)
 
-    }
-    
+        /* //moving notification btn by dragging
+        var cursorX;
+        var cursorY;
+        var isMouseDown; //0 for up 1 for down
 
-   
+        $("#notifBtn").mousedown(function () {
+            isMouseDown = true;
+        })
+
+        $("#notifBtn").mouseup(function () {
+            isMouseDown = false;
+        })
+
+        document.onmousemove = function (e) {
+            cursorX = e.pageX - 20;
+            cursorY = e.pageY - 20;
+            if (cursorX <= 0)
+                cursorX = 0;
+            if (cursorY <= 50)
+                cursorY = 50;
+            if (cursorX >= 310)
+                cursorX = 310;
+            if (cursorY >= 590)
+                cursorY = 590;
+            if (isMouseDown) {
+                $("#notifBtn").css("left", cursorX + "px");
+                $("#notifBtn").css("top", cursorY + "px");
+            }
+        }
+
+        function allowDrag(e) {
+            e.preventDefault();
+        }*/
+    }
 })();
 
-  
